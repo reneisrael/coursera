22c632634290241c5241677aafa4c0d647d06da6
reneisrael <reneisrael@users.noreply.github.com>
2015-11-06 19:43:16 -0600

    Create Rock-paper-scissors-lizard-Spock.py

---
+++ Rock-paper-scissors-lizard-Spock.py
@@ -0,0 +1,68 @@
+# Rock-paper-scissors-lizard-Spock
+
+import random
+
+# helper functions
+
+def name_to_number(name):
+    if name == "rock":
+        name = 0
+    elif name == "Spock":
+        name = 1
+    elif name == "paper":
+        name = 2
+    elif name == "lizard":
+        name = 3
+    elif name == "scissors":
+        name = 4
+    else: 
+        print "No correct name given:"
+        
+    return name
+  
+def number_to_name(number):
+    if number == 0:
+        number = "rock"
+    elif number == 1:
+        number = "Spock"
+    elif number == 2:
+        number = "paper"
+    elif number == 3:
+        number = "lizard"
+    elif number == 4:
+        number = "scissors"
+    else:
+        print "No correct number given:"
+    return number
+ 
+def rpsls(player_choice): 
+    # print a blank line to separate consecutive games
+    print
+    # print out the message for the player's choice
+    print "Player chooses", player_choice
+    # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)
+    # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0,5)
+    # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
+    # print out the message for computer's choice
+    print "Computer chooses", comp_choice
+    # compute difference of comp_number and player_number modulo five
+    a = (player_number - comp_number) % 5
+    # use if/elif/else to determine winner, print winner message
+    if player_number == comp_number :
+        print "Player and computer tie!"
+    elif a <= 2:
+        print "Player wins!"
+    else :
+        print "Computer wins!"
+    
+# test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
+
+# always remember to check your completed program against the grading rubric
